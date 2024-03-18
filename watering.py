35279fded0fb03d23d9f092073152e6b2295baed
Antoine Mouchère <a.mouchere@gmail.com>
2024-03-18 23:27:36 +0100

    :sparkles: Add http API way to launch the watering

--- watering.py
+++ watering.py
@@ -1,9 +1,11 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
 import threading, logging, json, os, sys
 import pytz, requests
 import RPi.GPIO as GPIO
 import time
 from gpiozero import Button
 from datetime import datetime
+from urllib.parse import urlparse, parse_qs
 
 
 logging.basicConfig(filename='/home/pi/growlab-watering/watering.log', level=logging.INFO, format='%(asctime)s %(message)s')
@@ -22,6 +24,39 @@ try:
 except Exception as e:
     logging.error("Error: {}".format(e))
     sys.exit(1)
+
+def start_server():
+    port = config["server"]["port"]
+    logging.info("--- Start server on port {} ---".format(port))
+    
+    server_address = ('', port)
+    httpd = HTTPServer(server_address, RequestHandler)
+    httpd.serve_forever()
+
+class RequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        parsed_path = urlparse(self.path)
+        query_params = parse_qs(parsed_path.query)
+
+        if parsed_path.path == '/api/watering' and 'pump' in query_params:
+            pump_param = query_params['pump'][0]
+            pump_number = int(pump_param)
+            name = config["pumps"][pump_number]["name"]
+            relay_gpio = config["pumps"][pump_number]["relay_gpio"]
+            duration = config["pumps"][pump_number]["duration"]
+            logging.info("{} -> API call".format(name))
+            
+            relayOnForSeconds(name, relay_gpio, duration)
+
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            self.wfile.write(json.dumps({'message': 'Watering started for pump {}'.format(pump_number)}).encode('utf-8'))
+        else:
+            self.send_response(400)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            self.wfile.write(json.dumps({'error': 'Invalid request. Please provide pump number in query parameter. Ex: /api/watering?pump=2'}).encode('utf-8'))
 
 
 def relayOnForSeconds(name, relay_gpio, duration):
@@ -74,3 +109,8 @@ for pump in config["pumps"]:
     thread2 = threading.Thread(target=infiniteloop2, args=(name, relay_gpio, button, duration))
     thread2.start()
 
+if config["server"]["isActive"] == True:
+    server_thread = threading.Thread(target=start_server)
+    server_thread.start()
+else:
+    logging.info("--- Server feature is disabled ---")
